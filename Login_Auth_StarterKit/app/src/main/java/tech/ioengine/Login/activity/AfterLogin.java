86f173eed243124b1a0615e2133fe04ff8264674
chichikolon <djimgoupat@yahoo.fr>
2018-03-18 12:16:15 +0100

    update Module

---
+++ Login_Auth_StarterKit/app/src/main/java/tech/ioengine/Login/activity/AfterLogin.java
@@ -0,0 +1,8 @@
+package tech.ioengine.Login.activity;
+
+/**
+ * Created by allwithoutblue on 3/17/18.
+ */
+
+public class AfterLogin {
+}
---
+++ Login_Auth_StarterKit/app/src/main/java/tech/ioengine/Login/activity/SplaschScreen.java
@@ -0,0 +1,42 @@
+package tech.ioengine.Login.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+/**
+ * Created by chichikolon on 07.12.2017.
+ */
+
+public class SplaschScreen extends AppCompatActivity{
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+                ,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(tech.ioengine.Login.R.layout.splaschscreen);
+
+
+
+        handler =  new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplaschScreen.this, AfterLogin.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },6000);
+
+
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+}
